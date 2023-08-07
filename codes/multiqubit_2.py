2897ba176999549b104c5585ee87dab6f71e18ed
Akash Kundu <kunduaku339@gmail.com>
2023-08-07 17:09:26 +0200

    scipy 2 qubit YAQQ

--- codes/multiqubit_2.py
+++ codes/multiqubit_2.py
@@ -12,6 +12,8 @@
 
 from qiskit.circuit.library import XGate,ZGate,YGate
 from qiskit.quantum_info import Pauli, Choi, process_fidelity
+from scipy.optimize import minimize  
+
 # from math import pi
 import numpy as np
 
@@ -59,7 +61,6 @@ def rand_2q_decomp(U, random_trail):
 
     choi0 = Choi(UnitaryGate(U))
     pfi_best = 0
-    dep_best = 0
     for i in range(random_trail):
         list_ang = np.random.random(12)*2*np.pi
         t = np.random.random(3)
@@ -82,6 +83,40 @@ def rand_2q_decomp(U, random_trail):
             qcirc_best_list = [U31, U32, majhe_eso, U33, U34]
             print(i, pfi_best)
     return pfi_best, qcirc_best_list
+
+
+
+def scipy_2q_decomp(U, random_trail):
+
+    choi0 = Choi(UnitaryGate(U))
+
+    cost_list, ang_list, t_list = [], [], []
+
+    for _ in range(random_trail):
+        t = np.random.random(3)
+        tx, ty, tz = t[0], t[1], t[2]
+        def cost_to_optimize(list_ang):
+            U31, U32 = qiskit_U3(list_ang[0], list_ang[1], list_ang[2]), qiskit_U3(list_ang[3], list_ang[4], list_ang[5]),
+            U33, U34 = qiskit_U3(list_ang[6], list_ang[7], list_ang[8]),qiskit_U3(list_ang[9], list_ang[10], list_ang[11])
+
+            majhe_eso = Can(tx,ty,tz)
+            peeche_hat = np.kron(U31, U32)
+            egia_cholo = np.kron(U33, U34)
+            prodhan_gate = np.matmul(egia_cholo, np.matmul(majhe_eso, peeche_hat))
+            choi01 = Choi(UnitaryGate(prodhan_gate))
+
+            return -process_fidelity(choi0,choi01)
+        list_ang = np.random.random(12)*2*np.pi
+        res = minimize(cost_to_optimize, list_ang, method = 'COBYLA', options={'maxiter': 1000})
+        cost_list.append(res['fun'])
+        ang_list.append(res['x'])
+        t_list.append([tx,ty,tz])
+        print(res['fun'])
+    best_GS = cost_list.index(np.min(cost_list))
+    sort_opt_ang = ang_list[best_GS]
+    opt_t = t_list[best_GS]
+        
+    return sort_opt_ang, opt_t
         
 
 
@@ -95,7 +130,28 @@ if __name__ == "__main__":
     
     rand_U = gen_ds_randU(samples = samples, dimensions = dim)
     rand_U0 = rand_U[0]
-    rand_trial = 100000
-    rand_2q_decomp(rand_U0, rand_trial)
+    choi0 = Choi(UnitaryGate(rand_U0))
+    rand_trial = 10
+    # rand_2q_decomp(rand_U0, rand_trial)
+    best_ang, best_t = scipy_2q_decomp(rand_U0, rand_trial)
+    list_ang = best_ang
+    t = best_t
+    tx, ty, tz = t[0], t[1], t[2]
+    # print(list_ang)
+    U31, U32 = qiskit_U3(list_ang[0], list_ang[1], list_ang[2]), qiskit_U3(list_ang[3], list_ang[4], list_ang[5]),
+    U33, U34 = qiskit_U3(list_ang[6], list_ang[7], list_ang[8]),qiskit_U3(list_ang[9], list_ang[10], list_ang[11])
+
+    majhe_eso = Can(tx,ty,tz)
+    peeche_hat = np.kron(U31, U32)
+    egia_cholo = np.kron(U33, U34)
+    prodhan_gate = np.matmul(egia_cholo, np.matmul(majhe_eso, peeche_hat))
+    print('----------------------------')
+    print(rand_U0)
+    print()
+    print(prodhan_gate)
+    print('----------------------------')
+
+    choi01 = Choi(UnitaryGate(prodhan_gate))
+    print(process_fidelity(choi0,choi01))
 
 
