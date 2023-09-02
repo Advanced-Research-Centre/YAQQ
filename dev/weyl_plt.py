04c2c21bfed8e6680e0306db9302886b44f7b6e2
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2023-09-02 14:22:25 +0200

    weyl plot

--- dev/weyl_plt.py
+++ dev/weyl_plt.py
@@ -1,18 +1,4 @@
-
-# import matplotlib.pyplot as plt
-
-# from mpl_toolkits.mplot3d import Axes3D  # required to activate 2D plotting
-# from mpl_toolkits.mplot3d import proj3d
-# from matplotlib.patches import FancyArrowPatch
-
-# cx = 0.5
-# cy = 0.5
-# cz = 0.5
-
-# plt.plot(cx,cy,cz,'o')
-# plt.show()
-
-
+import weylchamber
 from itertools import product
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,7 +8,7 @@ import math
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-px = 6
+px = 23
 cx = np.linspace(0, 1, px)
 cy = np.linspace(0, 1, px)
 cz = np.linspace(0, 1, px)
@@ -32,12 +18,39 @@ def rgb_to_hex(r, g, b):
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
 for i in gs:
-    g = list(i)
-    # ax.scatter(g[0], g[1], g[2], c=rgb_to_hex(int(g[0]*255),int(g[1]*255),int(g[2]*255)))
-    ax.scatter(g[0], g[1], g[2], c=rgb_to_hex(int(255*(math.sin(g[0]*math.pi)+1)/2),int(255*(math.sin(g[1]*math.pi)+1)/2),int(255*(math.sin(g[2]*math.pi)+1)/2)))
+    g = list(i)    
+    if weylchamber.point_in_weyl_chamber(g[0], g[1], g[2]):
+        # ax.scatter(g[0], g[1], g[2], c=rgb_to_hex(int(g[0]*255),int(g[1]*255),int(g[2]*255)))
+        ax.scatter(g[0], g[1], g[2], s=5,c=rgb_to_hex(int(255*(math.sin(g[0]*math.pi)+1)/2),int(255*(math.sin(g[1]*math.pi)+1)/2),int(255*(math.sin(g[2]*math.pi)+1)/2)))
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+ax.plot3D([0,0.5],[0,0.5],[0,0.5],linestyle='--',color='black')       # Isotropic exchange Swap-alpha gates
+ax.plot3D([1,0.5],[0,0.5],[0,0.5],linestyle='--',color='black')       # Isotropic exchange Swap-alpha gates 
+ax.plot3D([0.5,0.5],[0.5,0.5],[0,0.5],linestyle='--',color='black')   # Parameterized Swap gates
+ax.plot3D([0,1],[0,0],[0,0],linestyle='--',color='black')             # Ising gates
+ax.plot3D([0,0.5],[0,0.5],[0,0],linestyle='--',color='black')         # XY gates
+ax.plot3D([1,0.5],[0,0.5],[0,0],linestyle='--',color='black')         # XY gates
+
+ax.plot3D([0.25,0.75],[0.25,0.25],[0.25,0.25],linestyle=':',color='grey')
+ax.plot3D([0.25,0.5],[0.25,0],[0.25,0],linestyle=':',color='grey')
+ax.plot3D([0.5,0.75],[0,0.25],[0,0.25],linestyle=':',color='grey')
+ax.plot3D([0.25,0.25],[0.25,0.25],[0.25,0],linestyle=':',color='grey')
+ax.plot3D([0.75,0.75],[0.25,0.25],[0.25,0],linestyle=':',color='grey')
+ax.plot3D([0.5,0.25],[0,0.25],[0,0],linestyle=':',color='grey')
+ax.plot3D([0.5,0.75],[0,0.25],[0,0],linestyle=':',color='grey')
+ax.plot3D([0.25,0.5],[0.25,0.5],[0.25,0],linestyle=':',color='grey')
+ax.plot3D([0.75,0.5],[0.25,0.5],[0.25,0],linestyle=':',color='grey')
+
+
+ax.set_xlim(0, 1)
+ax.set_ylim(0, 1)
+ax.set_zlim(0, 1)
+ax.xaxis.set_ticks([0, 0.25, 0.5, 0.75, 1])
+ax.yaxis.set_ticks([0, 0.25, 0.5, 0.75, 1])
+ax.zaxis.set_ticks([0, 0.25, 0.5, 0.75, 1])
+ax.set_xlabel('$c_1/\pi$')
+ax.set_ylabel('$c_2/\pi$')
+ax.set_zlabel('$c_3/\pi$')
+
+plt.title('Weyl Chamber')
 
 plt.show()
