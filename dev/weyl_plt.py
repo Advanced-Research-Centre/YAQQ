c24bff8aadb5888b7acb5c434514bdb5d97f411a
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2023-09-02 21:26:54 +0200

    weyl ready

--- dev/weyl_plt.py
+++ dev/weyl_plt.py
@@ -19,9 +19,9 @@ def rgb_to_hex(r, g, b):
 
 for i in gs:
     g = list(i)    
-    if weylchamber.point_in_weyl_chamber(g[0], g[1], g[2]):
+    # if weylchamber.point_in_weyl_chamber(g[0], g[1], g[2]):
         # ax.scatter(g[0], g[1], g[2], c=rgb_to_hex(int(g[0]*255),int(g[1]*255),int(g[2]*255)))
-        ax.scatter(g[0], g[1], g[2], s=5,c=rgb_to_hex(int(255*(math.sin(g[0]*math.pi)+1)/2),int(255*(math.sin(g[1]*math.pi)+1)/2),int(255*(math.sin(g[2]*math.pi)+1)/2)))
+    ax.scatter(g[0], g[1], g[2], s=5,c=rgb_to_hex(int(255*(math.sin(g[0]*math.pi)+1)/2),int(255*(math.sin(g[1]*math.pi)+1)/2),int(255*(math.sin(g[2]*math.pi)+1)/2)))
 
 ax.plot3D([0,0.5],[0,0.5],[0,0.5],linestyle='--',color='black')       # Isotropic exchange Swap-alpha gates
 ax.plot3D([1,0.5],[0,0.5],[0,0.5],linestyle='--',color='black')       # Isotropic exchange Swap-alpha gates 
@@ -41,6 +41,13 @@ ax.plot3D([0.25,0.5],[0.25,0.5],[0.25,0],linestyle=':',color='grey')
 ax.plot3D([0.75,0.5],[0.25,0.5],[0.25,0],linestyle=':',color='grey')
 
 
+ax.view_init(elev=15, azim=-55)
+tmp_planes = ax.zaxis._PLANES 
+ax.zaxis._PLANES = ( tmp_planes[2], tmp_planes[3], 
+                     tmp_planes[0], tmp_planes[1], 
+                     tmp_planes[4], tmp_planes[5])
+
+
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
 ax.set_zlim(0, 1)
@@ -49,7 +56,14 @@ ax.yaxis.set_ticks([0, 0.25, 0.5, 0.75, 1])
 ax.zaxis.set_ticks([0, 0.25, 0.5, 0.75, 1])
 ax.set_xlabel('$c_1/\pi$')
 ax.set_ylabel('$c_2/\pi$')
-ax.set_zlabel('$c_3/\pi$')
+ax.zaxis.set_rotate_label(False) 
+ax.set_zlabel('$c_3/\pi$', rotation=90)
+
+ax.grid(False)
+
+ax.xaxis.pane.fill = False
+ax.yaxis.pane.fill = False
+ax.zaxis.pane.fill = False
 
 plt.title('Weyl Chamber')
 
