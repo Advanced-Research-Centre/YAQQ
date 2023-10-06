11a302a1acc09e72845188d650fea11b4c19d020
Akash Kundu <kunduaku339@gmail.com>
2023-10-06 17:27:22 +0200

    1 qubit experiments finished

--- dev/plot.py
+++ dev/plot.py
@@ -5,26 +5,33 @@ search = ['randsearch', 'scipy']
 gateset = ['p1h1t1', 'p1p1']
 # fig, ax = plt.subplots(1, 2, figsize = (7,4))
 # gateset = gateset[0]
-search, gateset = search[1], gateset[1]
-
+search, gateset = search[1], gateset[0]
+decomp = 'sk'
 # rand_search_pf1 = np.load(f'results/data/data_typ1_gateset_p1h1t1_iter100_time100_{search}_100_data_pf1.npy', allow_pickle=True)
 # rand_search_pf2 = np.load(f'results/data/data_typ1_gateset_p1h1t1_iter100_time100_{search}_100_data_pf2.npy', allow_pickle=True)
 # rand_search_cd1 = np.load(f'results/data/data_typ1_gateset_p1h1t1_iter100_time100_{search}_100_data_cd1.npy', allow_pickle=True)
 # rand_search_cd2 = np.load(f'results/data/data_typ1_gateset_p1h1t1_iter100_time100_{search}_100_data_cd2.npy', allow_pickle=True)
-
+# data_typ1_sk_decomp_p1p1_iter100_time100_scipy_50_1_1_1_0_pf2
 x = []
-for w1 in [50]:#range(0,200+1,50):
-    print('weight', [w1,50,1,1,0])
-    if w1 == 0:
-        rand_search_pf1 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_pf1.npy', allow_pickle=True)
-        rand_search_pf2 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_pf2.npy', allow_pickle=True)
-        rand_search_cd1 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_cd1.npy', allow_pickle=True)
-        rand_search_cd2 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_cd2.npy', allow_pickle=True)
-    else:
-        rand_search_pf1 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_pf1.npy', allow_pickle=True)
-        rand_search_pf2 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_pf2.npy', allow_pickle=True)
-        rand_search_cd1 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_cd1.npy', allow_pickle=True)
-        rand_search_cd2 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_cd2.npy', allow_pickle=True)
+for w1 in [50]:
+    print('weight', f'{decomp} decompostion', f'{gateset} gateset', 'weight:', [w1,1,1,1,0])
+    if decomp == 'sk':
+        rand_search_pf1 = np.load(f'results/data/data_typ1_sk_decomp_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_pf1.npy', allow_pickle=True)
+        rand_search_pf2 = np.load(f'results/data/data_typ1_sk_decomp_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_pf2.npy', allow_pickle=True)
+        rand_search_cd1 = np.load(f'results/data/data_typ1_sk_decomp_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_cd1.npy', allow_pickle=True)
+        rand_search_cd2 = np.load(f'results/data/data_typ1_sk_decomp_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_cd2.npy', allow_pickle=True)
+
+    elif decomp == 'random':
+        if w1 == 0:
+            rand_search_pf1 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_pf1.npy', allow_pickle=True)
+            rand_search_pf2 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_pf2.npy', allow_pickle=True)
+            rand_search_cd1 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_cd1.npy', allow_pickle=True)
+            rand_search_cd2 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_cd2.npy', allow_pickle=True)
+        else:
+            rand_search_pf1 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_pf1.npy', allow_pickle=True)
+            rand_search_pf2 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_pf2.npy', allow_pickle=True)
+            rand_search_cd1 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_cd1.npy', allow_pickle=True)
+            rand_search_cd2 = np.load(f'results/data/data_typ1_gateset_{gateset}_iter100_time100_{search}_{w1}_1_1_1_0_cd2.npy', allow_pickle=True)
 
 # ax[0].plot(rand_search_pf1, 'r-', label = '[H,T]')
 # # ax[0].plot(rand_search_pf2, 'b--', label = '$[P(\\theta),H,T]$')
@@ -39,8 +46,8 @@ for w1 in [50]:#range(0,200+1,50):
 
     print(f'gateset [H,T] average process fidelity ({search}):', np.mean(rand_search_pf1))
     print(f'gateset {gateset} average process fidelity ({search}):', np.mean(rand_search_pf2))
-    print(f'gateset [H,T] average process fidelity ({search}):', np.mean(rand_search_cd1))
-    print(f'gateset {gateset} average process fidelity ({search}):', np.mean(rand_search_cd2))
+    print(f'gateset [H,T] average depth ({search}):', np.mean(rand_search_cd1))
+    print(f'gateset {gateset} average depth ({search}):', np.mean(rand_search_cd2))
     x.append(np.mean(rand_search_pf2) - np.mean(rand_search_pf1))
     print()
 # plt.plot([0,50,100,150,200], x, '-o', label = '(pf2-pf1)')
