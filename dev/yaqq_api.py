5a22fd00769c60ef049f54c095db3fbaf99ba644
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2024-02-09 18:48:05 +0100

    more novelty

--- dev/yaqq_api.py
+++ dev/yaqq_api.py
@@ -6,7 +6,7 @@ from yaqq import yaqq_dev
 
 for i in range(0,19):
     print("\n\n\nTRIAL ", i)
-    yaqq_dev('NUSA_eid-0011')
-    # yaqq_dev('NUSA_eid-0012')
-    # yaqq_dev('NUSA_eid-0013')
-    # yaqq_dev('NUSA_eid-0015')
+    # yaqq_dev('NUSA_eid-0011')
+    yaqq_dev('NUSA_eid-0012')
+    yaqq_dev('NUSA_eid-0013')
+    yaqq_dev('NUSA_eid-0015')
