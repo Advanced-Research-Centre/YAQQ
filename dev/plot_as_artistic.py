a1b38e72b22c12d7a2342aac91a2499d27eb4a8b
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2024-02-19 18:42:38 +0100

    plots with tex

--- dev/plot_as_artistic.py
+++ dev/plot_as_artistic.py
@@ -1,10 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcParams.update(
-    {"font.family": "serif", "font.size": 18}
-)
-
 gs1_gates = 'original gate set'
 gs2_gates = 'novel gate set'
 
@@ -76,8 +72,12 @@ std_dep_gs02 = np.std(cd2)
 
 """ Code for plots"""
 
+plt.rcParams.update(
+    {"text.usetex": True, "font.family": "serif", "font.size": 20}
+)
+
 fs = 24
-ls = 1
+ls = 2
 ms = 5
 
 figg, ax = plt.subplots(1, 2)
@@ -86,8 +86,8 @@ ax[0].plot(pf2, '-x', markersize=ms, linewidth=ls, color = 'r', label = 'PF ['+g
 
 ax[0].plot(ivt_fid_gs01, '-s', markersize=ms, linewidth=ls, color = 'g', label = 'target PF trend')
 
-ax[0].axhline(y=avg_fid_gs01, linestyle='-.', linewidth=2, color = 'b' , label = 'avg.PF ['+gs1_gates+']')
-ax[0].axhline(y=avg_fid_gs02, linestyle='-', linewidth=2, color = 'r' , label = 'avg.PF ['+gs2_gates+']')
+ax[0].axhline(y=avg_fid_gs01, linestyle='-.', linewidth=2, color = 'b' , label = '$$\\langle \\textrm{PF} \\rangle \; [\\textrm{original gate set}]$$')
+ax[0].axhline(y=avg_fid_gs02, linestyle='-', linewidth=2, color = 'r' , label = '$$\\langle \\textrm{PF} \\rangle \; [\\textrm{novel gate set}]$$')
 
 ax[0].fill_between(range(0,len(pf1)), np.repeat(avg_fid_gs01+std_fid_gs01,len(pf1)), np.repeat(avg_fid_gs01-std_fid_gs01,len(pf1)), color='b', alpha=0.2)
 ax[0].fill_between(range(0,len(pf2)), np.repeat(avg_fid_gs02+std_fid_gs02,len(pf2)), np.repeat(avg_fid_gs02-std_fid_gs02,len(pf2)), color='r', alpha=0.2)
@@ -96,8 +96,8 @@ ax[0].fill_between(range(0,len(pf2)), np.repeat(avg_fid_gs02+std_fid_gs02,len(pf
 ax[1].plot(cd1, '-o', markersize=ms, linewidth=ls, color = 'b', label = 'CD ['+gs1_gates+']')
 ax[1].plot(cd2, '-x', markersize=ms, linewidth=ls, color = 'r', label = 'CD ['+gs2_gates+']')
 
-ax[1].axhline(y=avg_dep_gs01, linestyle='-.', linewidth=2, color = 'b', label = 'avg.CD ['+gs1_gates+']')
-ax[1].axhline(y=avg_dep_gs02, linestyle='-', linewidth=2, color = 'r', label = 'avg.CD ['+gs2_gates+']')
+ax[1].axhline(y=avg_dep_gs01, linestyle='-.', linewidth=2, color = 'b', label = '$$\\langle \\textrm{CD} \\rangle \; [\\textrm{original gate set}]$$')
+ax[1].axhline(y=avg_dep_gs02, linestyle='-', linewidth=2, color = 'r', label = '$$\\langle \\textrm{CD} \\rangle \; [\\textrm{novel gate set}]$$')
 
 ax[1].fill_between(range(0,len(cd1)), np.repeat(avg_dep_gs01+std_dep_gs01,len(cd1)), np.repeat(avg_dep_gs01-std_dep_gs01,len(cd1)), color='b', alpha=0.2)
 ax[1].fill_between(range(0,len(cd2)), np.repeat(avg_dep_gs02+std_dep_gs02,len(cd2)), np.repeat(avg_dep_gs02-std_dep_gs02,len(cd2)), color='r', alpha=0.2)
@@ -108,16 +108,17 @@ ax[1].set_ylabel("Circuit depth (CD)", fontsize=fs, fontfamily='serif')
 ax[0].set_xlabel("Data set of target unitary matrices", fontsize=fs, fontfamily='serif')
 ax[1].set_xlabel("Data set of target unitary matrices", fontsize=fs, fontfamily='serif')
 
-ax[0].set_xlim([0, len(pf1)-1])
-ax[1].set_xlim([0, len(cd1)-1])
+ax[0].set_xlim([-1, len(pf1)])
+ax[1].set_xlim([-1, len(cd1)])
 
 ax[0].set_ylim(bottom=-0.1,top=1.1)    # not set to show std. div.
 # ax[1].set_ylim(bottom=0,top=None) # not set to show std. div.
-ax[0].legend(loc='lower right')
+ax[0].legend(loc='center right')
 ax[1].legend(loc='lower right')
 
 # figg.set_size_inches(18,8)
 figg.set_size_inches(26,8)
 figg.subplots_adjust(left=0.06, right=0.995, top=0.98, bottom=0.098, wspace=0.183, hspace=0.2)
 
-plt.show()
+plt.savefig('results/figures_for_paper/artistic.pdf')
+# plt.show()
--- dev/plot_as_correlation.py
+++ dev/plot_as_correlation.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
-import re
-import os
+
+# import re
+# import os
 
 # dir_res = 'results/data/'
 
@@ -21,11 +22,13 @@ import os
 
 # NUSA_eid-0013_2024-02-10-10-55pf1.npy -0.42662448519333446
 
+fs = 16
+
 plt.rcParams.update(
-    {"text.usetex": True, "font.family": "serif", "font.size": 12}
+    {"text.usetex": True, "font.family": "serif", "font.size": 14}
 )
 
-fig, (ax1,ax2) = plt.subplots(2, 1, figsize=(4,4), dpi = 100)
+fig, (ax1,ax2) = plt.subplots(2, 1, figsize=(6,6), dpi = 200)
 
 data = 'NUSA_eid-0013_2024-02-10-10-55'
 y1, y2, y3 = [], [], []
@@ -40,14 +43,18 @@ for smaller_pojnts in range(2,50+1,1):
     y3.append(np.mean(pf_ht))
     y2.append(pearsonr(pf_ht, pf_p1p1)[0])
 y1 = np.load(f'results/data/{data}pf1.npy')
-ax2.set_xlabel('Data set of target unitary', fontsize = 9)
+ax2.set_xlabel('Data set of target unitary matrices', fontsize = fs)
 ax1.set_xticks([])
-ax2.set_ylabel('Pearson correlation', fontsize = 9)
-ax1.set_ylabel('Process fidelity (PF)', fontsize = 9)
+ax2.set_ylabel('Pearson correlation', fontsize = fs)
+ax1.set_ylabel('Process fidelity (PF)', fontsize = fs)
 ax1.plot(y1, 'ob', markersize = 3, alpha = 0.5, label = 'PF')
-ax1.plot(y3, '-r', label = '$$\\textrm{Cumulative}\;\\langle PF\\rangle$$')
-ax2.plot(y2)
-ax1.legend(fontsize = 8)
+ax1.plot(y3, '-r', label = '$$\\textrm{Cumulative}\;\\langle \\textrm{PF}\\rangle$$')
+ax2.plot(y2, '-b')
+ax2.set_ylim(-1.0,0)
+ax1.set_ylim(0,1)
+ax1.set_xlim(0,50)
+ax2.set_xlim(0,50)
+ax1.legend(fontsize = fs)
 fig.tight_layout()
-# plt.savefig('correlation_plot.pdf')
+plt.savefig('results/figures_for_paper/correlation_plot.pdf')
 plt.show()
